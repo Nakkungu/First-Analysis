a16ff69eb4bf00c4cec6e82e5833cb2de4639149
Angella Nakkungu <64511665+Nakkungu@users.noreply.github.com>
2024-04-14 17:22:34 +0300

    All files

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
+
+
+st.set_page_config(
+    page_title='Login',
+    page_icon='🪵',
+    layout='wide'
+)
+
+with st.expander("Columns of Interest Descriptions", expanded=True):
+            st.info("""
+            - **NEAR_DIST**: Distance between the assessed site in the database and the nearest remotely identified site
+            - **metric**: Measure of impact for remotely identified site
+            - **Population**: Population count in !km radius of assessed site
+            - **GPS_Latit**: GPS latitude for assessed site
+            - **GPS_Longi**: GPS longitude for assessed site
+                    """)
